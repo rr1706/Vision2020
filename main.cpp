e5febfb8d4506ac62d0f51414682e07888bb4e88
Quant5 <quantum.b4@gmail.com>
2020-02-17 18:57:45 -0600

    Fix line endings and indention

--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@ VideoCapture camera;
 
 //constents: need to be changed when camera moves/replaced
 double tYOffset = 30;
-double camHeight =  22.25; //height off ground
-double focalLength = 457.5; //find new fl
-double px_per_mm = 1.52048426; //find
+double camHeight = 22.25;		//height off ground
+double focalLength = 457.5;		//find new fl
+double px_per_mm = 1.52048426;	//find
 //double focalLength = 309.15445743;
 //double robotCenter = 10.795;
 
@@ -38,7 +38,7 @@ double FovY = 72.05474677;
 int tMin = 170;
 
 //sets kernal to a cross, the shape of the kernal is determained by the shape of the target
-Mat kernel = (cv::Mat_<unsigned char>(3,3) << 1,0,1,0,1,0,1,0,1); //look for new kernal
+Mat kernel = (cv::Mat_ < unsigned char >(3, 3) << 1, 0, 1, 0, 1, 0, 1, 0, 1);	//look for new kernal
 
 void runCamera(Mat base);
 
@@ -51,37 +51,35 @@ char esc;
 };*/
 
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	while(!utils::fs::exists("/dev/video0")){
+	while (!utils::fs::exists("/dev/video0")) {
 		usleep(500);
 	}
 
 	//loadConfig("../VisionSettings.conf");
 	system("/usr/local/bin/setCam.sh");
-	#ifdef NETWORK
+#ifdef NETWORK
 	startTable();
-	#endif
+#endif
 	camera.open(0);
-	#ifdef NETWORK
+#ifdef NETWORK
 	sendString("On?", "Yes");
-	#endif
+#endif
 	//display with with camera
 	Mat base;
-	while(camera.isOpened())
-	{
+	while (camera.isOpened()) {
 		camera >> base;
 		//flip(base, base, 0); //only needed if cam is upside down
 		runCamera(base);
 		esc = waitKey(33);
-		if (esc == 27)
-		{
+		if (esc == 27) {
 			break;
 		}
 	}
-	#ifdef NETWORK
+#ifdef NETWORK
 	sendString("On?", "No");
-	#endif
+#endif
 	camera.release();
 	return 0;
 }
@@ -92,44 +90,46 @@ void runCamera(Mat base)
 	Mat threshed;
 
 	//used in contours
-	vector<vector<Point2i>> contours;
-	vector<Vec4i> hierarchy;
+	vector < vector < Point2i >> contours;
+	vector < Vec4i > hierarchy;
 
 	//try canny
-	inRange(base,Scalar(tMin,tMin,tMin),Scalar(255,255,255),threshed);
+	inRange(base, Scalar(tMin, tMin, tMin), Scalar(255, 255, 255),
+			threshed);
 	//threshold(base,threshed,tMin,255,THRESH_BINARY);
 	erode(threshed, threshed, kernel);
 	dilate(threshed, threshed, kernel);
 	//Canny(base, threshed, 50, 100);
 
-    //contours
-	findContours(threshed, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	#ifdef WITH_HEAD
+	//contours
+	findContours(threshed, contours, hierarchy, RETR_EXTERNAL,
+				 CHAIN_APPROX_SIMPLE);
+#ifdef WITH_HEAD
 	drawContours(base, contours, -1, Scalar(0, 255, 0), 1);
-	#endif
+#endif
 
 	//optimize contour auto thresh
-	if(contours.size()<1){
+	if (contours.size() < 1) {
 		tMin--;
 	}
-	vector<vector<Point2i>> hull(contours.size());
+	vector < vector < Point2i >> hull(contours.size());
 	//only complete if a countour is found
-	for(size_t i = 0; i <  contours.size(); i++){
+	for (size_t i = 0; i < contours.size(); i++) {
 
-		vector<Point2i> contour = contours[i];
+		vector < Point2i > contour = contours[i];
 
-		if(contourArea(contour) > 300){
-            tMin++;
-        }
-		if(contourArea(contour) < 70 || contours.size() < 1){
-            tMin--;
+		if (contourArea(contour) > 300) {
+			tMin++;
 		}
-		if(contourArea(contours[i]) < 1000){
+		if (contourArea(contour) < 70 || contours.size() < 1) {
+			tMin--;
+		}
+		if (contourArea(contours[i]) < 1000) {
 
 			convexHull(contours[i], hull[i]);
-			#ifdef WITH_HEAD
-			drawContours(base, hull, (int)i, Scalar(255,0,255));
-			#endif
+#ifdef WITH_HEAD
+			drawContours(base, hull, (int) i, Scalar(255, 0, 255));
+#endif
 
 			//based off https://www.pyimagesearch.com/2016/02/08/opencv-shape-detection/
 			//vector<double> approx;
@@ -138,63 +138,72 @@ void runCamera(Mat base)
 			//cout << "approx points: " << to_string(approx.size()) << endl;
 
 			//if(approx.size == 4){
-				
-				//bounding box
-				Rect bound = boundingRect(contours[i]);
-				#ifdef WITH_HEAD
-				RotatedRect rotRect = minAreaRect(contours[i]);
-				rectangle(base, bound.tl(), bound.br(), Scalar(255, 0, 0), 5);
-				#endif
-				//finds target center and places crosshair
-				Point2f centerOfTarget = Point(bound.x+bound.width/2, bound.y+bound.height/2);
-				#ifdef WITH_HEAD
-				drawMarker(base, Point(centerOfTarget), Scalar(255, 0, 0), MARKER_CROSS, 20, 5);
-				#endif
-				//Draw crosshair on the center of the image
-				int imgWidth = base.cols;
-				int imgHeight = base.rows;
-				double pxWidth = (double) bound.width;
-				#ifdef WITH_HEAD
-				drawMarker(base, Point(imgWidth / 2, imgHeight / 2), Scalar(255, 0, 0), MARKER_CROSS, 20, 5);
-				#endif
 
-				//find and send values
-				//double tY = calculateTY(imgHeight, centerOfTarget, FovY); //find new ty formula
-				double Xrot = calculateXrot(imgWidth, centerOfTarget, FovX); // maybe find new tx formula
-				double distToTarget = findDistance2(widthOfHex, pxWidth, focalLength, px_per_mm); //finish
+			//bounding box
+			Rect bound = boundingRect(contours[i]);
+#ifdef WITH_HEAD
+			RotatedRect rotRect = minAreaRect(contours[i]);
+			rectangle(base, bound.tl(), bound.br(), Scalar(255, 0, 0), 5);
+#endif
+			//finds target center and places crosshair
+			Point2f centerOfTarget = Point(bound.x + bound.width / 2,
+										   bound.y + bound.height / 2);
+#ifdef WITH_HEAD
+			drawMarker(base, Point(centerOfTarget), Scalar(255, 0, 0),
+					   MARKER_CROSS, 20, 5);
+#endif
+			//Draw crosshair on the center of the image
+			int imgWidth = base.cols;
+			int imgHeight = base.rows;
+			double pxWidth = (double) bound.width;
+#ifdef WITH_HEAD
+			drawMarker(base, Point(imgWidth / 2, imgHeight / 2),
+					   Scalar(255, 0, 0), MARKER_CROSS, 20, 5);
+#endif
 
-				cout << "contour area: " << to_string(contourArea(contour)) << endl;
-				cout << "Thresh value: " << to_string(tMin) << endl;
-				if(distToTarget < 500){
-					cout << "Distance to target: " + to_string(distToTarget) << endl;
-					#ifdef WITH_HEAD
-					putText(base, "Distance: " + to_string(distToTarget), Point(30,30), FONT_HERSHEY_COMPLEX, 5, Scalar(255,50,200));
-					#endif
-					#ifdef NETWORK
-					sendDouble("Distance", distToTarget);
-					#endif
-				}
-				/*cout << "tY: " << to_string(tY) << endl;
-				#ifdef Head
-				putText(base, "tY: " + to_string(tY), Point(60,60), FONT_HERSHEY_COMPLEX, 5, Scalar(255,50,200));
-				#endif
-				#ifdef NETWORK
-				sendDouble("tY", tY);
-				#endif*/
-				cout << "Xrot: " << to_string(Xrot) << endl;
-				#ifdef WITH_HEAD
-				putText(base, "Xrot: " + to_string(Xrot), Point(90,90), FONT_HERSHEY_COMPLEX, 5, Scalar(255,50,200));
-				#endif
-				#ifdef NETWORK
-				sendDouble("Xrot", Xrot);
-				#endif
-				cout << " " << endl;
+			//find and send values
+			//double tY = calculateTY(imgHeight, centerOfTarget, FovY); //find new ty formula
+			double Xrot = calculateXrot(imgWidth, centerOfTarget, FovX);	// maybe find new tx formula
+			double distToTarget = findDistance2(widthOfHex, pxWidth, focalLength, px_per_mm);	//finish
+
+			cout << "contour area: " << to_string(contourArea(contour)) <<
+				endl;
+			cout << "Thresh value: " << to_string(tMin) << endl;
+			if (distToTarget < 500) {
+				cout << "Distance to target: " +
+					to_string(distToTarget) << endl;
+#ifdef WITH_HEAD
+				putText(base, "Distance: " + to_string(distToTarget),
+						Point(30, 30), FONT_HERSHEY_COMPLEX, 5, Scalar(255,
+																	   50,
+																	   200));
+#endif
+#ifdef NETWORK
+				sendDouble("Distance", distToTarget);
+#endif
+			}
+			/*cout << "tY: " << to_string(tY) << endl;
+			   #ifdef Head
+			   putText(base, "tY: " + to_string(tY), Point(60,60), FONT_HERSHEY_COMPLEX, 5, Scalar(255,50,200));
+			   #endif
+			   #ifdef NETWORK
+			   sendDouble("tY", tY);
+			   #endif */
+			cout << "Xrot: " << to_string(Xrot) << endl;
+#ifdef WITH_HEAD
+			putText(base, "Xrot: " + to_string(Xrot), Point(90, 90),
+					FONT_HERSHEY_COMPLEX, 5, Scalar(255, 50, 200));
+#endif
+#ifdef NETWORK
+			sendDouble("Xrot", Xrot);
+#endif
+			cout << " " << endl;
 			//}
 		}
 	}
 	//show final images
-	#ifdef WITH_HEAD
+#ifdef WITH_HEAD
 	imshow("Normal", base);
 	imshow("Thresh", threshed);
-	#endif
+#endif
 }
