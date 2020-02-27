48bfc66665a719003d11ad460a528d44560bcedf
Quant5 <quantum.b4@gmail.com>
2020-02-26 19:29:08 -0600

    maybe now

---
+++ cbp/CMakeFiles/vision2020.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vision/Documents/Vision2020/Functions.cpp" "/home/vision/Documents/Vision2020/cbp/CMakeFiles/vision2020.dir/Functions.cpp.o"
+  "/home/vision/Documents/Vision2020/Network.cpp" "/home/vision/Documents/Vision2020/cbp/CMakeFiles/vision2020.dir/Network.cpp.o"
+  "/home/vision/Documents/Vision2020/WebStreamTest.cpp" "/home/vision/Documents/Vision2020/cbp/CMakeFiles/vision2020.dir/WebStreamTest.cpp.o"
+  "/home/vision/Documents/Vision2020/main.cpp" "/home/vision/Documents/Vision2020/cbp/CMakeFiles/vision2020.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WITH_CUDA"
+  "WITH_HEAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  "/usr/local/wpilib/include/ntcore"
+  "/usr/local/wpilib/include/wpiutil"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
