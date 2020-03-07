2b52aa0604b785a230ac40d21a9fb5d438459d40
Quant5 <Quantum.b4@gmail.com>
2020-03-07 09:25:51 -0600

    testing thing

--- main.cpp
+++ main.cpp
@@ -38,17 +38,32 @@ void runCamera(cuda::GpuMat base);
 char esc;
 
 
+std::string get_tegra_pipeline(int width, int height, int fps) {
+    return "nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(width) + ", height=(int)" +
+            std::to_string(height) + ", format=(string)I420, framerate=(fraction)" + std::to_string(fps) +
+            "/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+}
+
 int main(int argc, char **argv)
 {
 	while (!utils::fs::exists("/dev/video0")) {
 		usleep(500);
 	}
 
+	// Options
+    int  WIDTH = 1920;
+    int  HEIGHT = 1080;
+    int FPS = 30;
+
+    // Define the gstream pipeline
+    std::string pipeline = get_tegra_pipeline(WIDTH, HEIGHT, FPS);
+    std::cout << "Using pipeline: \n\t" << pipeline << "\n";
+
 	system("/usr/local/bin/setCam.sh");
 	#ifdef WITH_NETWORK
 	startTable();
 	#endif
-	camera.open(0);
+	camera.open(pipeline ,CAP_GSTREAMER);
 	//display with with camera
 	Mat base;
 	cuda::GpuMat gbase, smol;
